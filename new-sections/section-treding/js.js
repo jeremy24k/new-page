120a3f5b10ab3ba459987e0d62383b1408476a1e
jeremy24k <jeremysanchezsoto@gamil.com>
2023-06-20 12:21:12 -0400

    changes-treding

--- new-sections/section-treding/js.js
+++ new-sections/section-treding/js.js
@@ -1,8 +1,7 @@
 let TitleChange = document.getElementById('titles-chages');
 let text = TitleChange.querySelectorAll('p');
 let index = 1;
-let percentage = index * -50;
 
-setInterval(funtion(){
-    TitleChange.style.transform = "translateY(-50%)";
-},1000);
+// setInterval(()=> {
+//     TitleChange.classList.toggle('active')
+// },1000)
