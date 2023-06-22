4e0096b372e7c1ec1a045af847b3409b62b198da
jeremy24k <jeremysanchezsoto@gamil.com>
2023-06-22 18:29:00 -0400

    section finished

--- new-sections/section-treding/js.js
+++
@@ -1,7 +0,0 @@
-let TitleChange = document.getElementById('titles-chages');
-let text = TitleChange.querySelectorAll('p');
-let index = 1;
-
-// setInterval(()=> {
-//     TitleChange.classList.toggle('active')
-// },1000)
